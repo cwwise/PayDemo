85dae089fedc2bc53d3c9d7a4d66195e43968327
chenwei <wei18810109052@163.com>
2015-11-24 15:37:47 +0800

    初始化

---
+++ CWPayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CWPayDemo
+//
+//  Created by weiwei on 15/11/24.
+//  Copyright © 2015年 weiwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CWPayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CWPayDemo
+//
+//  Created by weiwei on 15/11/24.
+//  Copyright © 2015年 weiwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
